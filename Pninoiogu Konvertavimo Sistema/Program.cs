c32501eefd5b3799e700d27027c480f373daa86e
4Karolis <4karolis.t@gmail.com>
2022-03-30 20:02:14 +0300

    created

---
+++ Pninoiogu Konvertavimo Sistema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pninoiogu_Konvertavimo_Sistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
