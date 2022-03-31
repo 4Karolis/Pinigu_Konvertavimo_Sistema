fd926b6bef738d2f0401e7ad603f0893a7896d42
4Karolis <4karolis.t@gmail.com>
2022-04-01 01:38:24 +0300

    Updated!

--- Pninoiogu Konvertavimo Sistema/Program.cs
+++ Pninoiogu Konvertavimo Sistema/Program.cs
@@ -6,98 +6,113 @@ namespace Pninoiogu_Konvertavimo_Sistema
     {
         static void Main(string[] args)
         {
-            string[] valiutos = { "USD", "PLN", "BYR" };
-            //1 EUR = 1,1126 USD, 1 EUR = 4,6679 PLN, 1 EUR = 22288,8906 BYR pagal 22.03.30 duomenis
-            decimal[] valiutuKursai = { 1.1126m, 4.6679m, 22288.8906m };
-
-            //IKaKonvertuoti();
-            //Console.WriteLine("I ka noretumete komnvertuoti?");
-            //Console.WriteLine("[1] USD | [2] PLN | [3] BYR");
-            //int iKaKonvertuot = int.Parse(Console.ReadLine());
-            decimal iKaKonvertuot = IKaKonvertuoti();
-            switch (iKaKonvertuot)
+            bool iseiti = true;
+            while (iseiti)//pabandyt su try.parse
             {
-                case 1:
-                    decimal euruKiekis = IveskiteEuruKieki();
-                    decimal suma = Konverteris(euruKiekis, valiutuKursai[iKaKonvertuot]);
-                    Console.WriteLine(suma);
-                   // decimal usdKiekis = valiutuKursai * euruKiekis;
-                    //Console.WriteLine($"Jums priklauso: {usdKiekis} {valiutos[0]}");
-                    break;
-                case 2:
-                    Console.WriteLine("Iveskite euru kieki: ");
-                    decimal input2 = decimal.Parse(Console.ReadLine());
-                    decimal plnKiekis = valiutuKursai[1] * input2;
-                    Console.WriteLine($"Jums priklauso: {plnKiekis} {valiutos[1]}");
-                    break;
-                case 3:
-                    Console.WriteLine("Iveskite euru kieki: ");
-                    decimal input3 = decimal.Parse(Console.ReadLine());
-                    decimal byrKiekis = valiutuKursai[2] * input3;
-                    Console.WriteLine($"Jums priklauso: {byrKiekis} {valiutos[2]}");
-                    break;
-                default:
-                    Console.WriteLine("Blogai ivestas pasirinkimas!");
-                    break;
+                string[] valiutosSimbolis = { "USD", "PLN", "BYR" };
+                //1 EUR = 1,1126 USD, 1 EUR = 4,6679 PLN, 1 EUR = 22288,8906 BYR pagal 22.03.30 duomenis
+                decimal[] valiutuKursai = { 1.1126m, 4.6679m, 22288.8906m };
+
+                decimal iKaKonvertuot = IKaKonvertuoti();
+                switch (iKaKonvertuot)
+                {
+                    case 0:
+                        string simbolis0 = valiutosSimbolis[0];
+                        decimal euruKiekis = IveskiteEuruKieki();
+                        decimal suma = Konverteris(euruKiekis, valiutuKursai[0], valiutosSimbolis[0]);
+                        //Console.WriteLine($"Jums priklauso: {suma} {simbolis0}");
+                        AnyKeyToContinue();
+
+                        break;
+                    case 1:
+                        string simbolis1 = valiutosSimbolis[1];
+                        decimal euruKiekis1 = IveskiteEuruKieki();
+                        decimal suma1 = Konverteris(euruKiekis1, valiutuKursai[1], valiutosSimbolis[1]);
+                        //Console.WriteLine($"Jums priklauso: {suma1} {simbolis1}");
+                        AnyKeyToContinue();
+                        break;
+                    case 2:
+                        string simbolis2 = valiutosSimbolis[2];
+                        decimal euruKiekis2 = IveskiteEuruKieki();
+                        decimal suma2 = Konverteris(euruKiekis2, valiutuKursai[2], valiutosSimbolis[2]);
+                        //Console.WriteLine($"Jums priklauso: {suma2} {simbolis2}"); ;
+                        AnyKeyToContinue();
+                        break;
+                    case 3:
+                        iseiti = false;
+                        Console.Clear();
+                        Console.WriteLine("Aciu, kad naudojates musu paslaugomis");
+                        break;
+                    default:
+                        Console.WriteLine("Blogai ivestas pasirinkimas!");
+                        break;
+                }
             }
-            #region
-            //Console.WriteLine("I ka noretumete komnvertuoti?");
-            //Console.WriteLine("[1] USD | [2] PLN | [3] BYR");
-            //int iKaKonvertuot = int.Parse(Console.ReadLine());
-            //switch (iKaKonvertuot)
-            //{
-            //    case 1:
-            //        Console.WriteLine("Iveskite euru kieki: ");
-            //        decimal input1 = decimal.Parse(Console.ReadLine());
-            //        decimal usdKiekis = valiutuKursai[0] * input1;
-            //        Console.WriteLine($"Jums priklauso: {usdKiekis} {valiutos[0]}");
-            //        break;
-            //    case 2:
-            //        Console.WriteLine("Iveskite euru kieki: ");
-            //        decimal input2 = decimal.Parse(Console.ReadLine());
-            //        decimal plnKiekis = valiutuKursai[1] * input2;
-            //        Console.WriteLine($"Jums priklauso: {plnKiekis} {valiutos[1]}");
-            //        break;
-            //    case 3:
-            //        Console.WriteLine("Iveskite euru kieki: ");
-            //        decimal input3 = decimal.Parse(Console.ReadLine());
-            //        decimal byrKiekis = valiutuKursai[2] * input3;
-            //        Console.WriteLine($"Jums priklauso: {byrKiekis} {valiutos[2]}");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Blogai ivestas pasirinkimas!");
-            //        break;
-            //}
-            #endregion
+
         }
-        //public static void JumsPriklauso(decimal input, string[] valiuta, decimal[] kursas)
+        #region METODAI
+        //public static decimal TryAgain()
         //{
-        //    input = IveskiteEuruKieki();
-        //    valiuta = string[] valiutos();
+        //    Console.WriteLine("           I ka noretumete komnvertuoti?");
+        //    Console.WriteLine("\n[0] USD | [1] PLN | [2] BYR | [3] ISEITI is programos");
+        //    return Console.Clear();
         //}
-        //public static void JumsPriklauso(decimal kiekis, decimal[] kursas, string[] simbolis)
-        //{ 
-        //    Console.WriteLine($"Jums priklauso: {kiekis * kursas[]} {}");
-        //}
-        public static decimal Konverteris(decimal euruKiekis, decimal valiutuKursai, string valiutos, decimal iKaKonvertuot)
+        public static void AnyKeyToContinue()
         {
-            euruKiekis = IveskiteEuruKieki();
+            Console.WriteLine("TESTI - Spauskite bet kuti kita mygtuka");
+            //int.TryParse(Console.ReadLine(), out int exit);
+            if (!int.TryParse(Console.ReadLine(), out int exit))
+            {
+                Console.Clear();
+            }
+            else
+            {
+                Console.Clear(); 
+            }
+        }
+        public static decimal Konverteris(decimal euruKiekis, decimal valiutuKursai, string valiutosSimbolis)
+        {            
+            string y = valiutosSimbolis; 
             decimal suma = euruKiekis * valiutuKursai;
-            //decimal priklauso = 
-            return suma;//Console.WriteLine($"Jums priklauso: {suma} {valiutos[iKaKonvertuot]}"); 
+            Console.WriteLine($"Jums priklauso: {suma} {y}");
+            //AnyKeyToContinue();
+            return suma; 
         }
         public static decimal IveskiteEuruKieki()
         {
             Console.WriteLine("Iveskite euru kieki: ");
-            decimal euruKiekis = decimal.Parse(Console.ReadLine());
-            return euruKiekis;
+            if (decimal.TryParse(Console.ReadLine(), out decimal euruKiekis))
+            {
+                return euruKiekis;
+            }
+            else
+            {
+                Console.WriteLine("Blogai ivestas pasirinkimas!");
+                return IveskiteEuruKieki(); 
+            }
         }
         public static decimal IKaKonvertuoti()
-        {
-            Console.WriteLine("I ka noretumete komnvertuoti?");
-            Console.WriteLine("[0] USD | [1] PLN | [2] BYR");
-            decimal iKaKonvertuot = decimal.Parse(Console.ReadLine());
-            return iKaKonvertuot;
+        {            
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("------------ PROGRAMA KONVERTUOJA EURUS -------------");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("           I ka noretumete komnvertuoti?");
+            Console.WriteLine("\n[0] USD | [1] PLN | [2] BYR | [3] ISEITI is programos");
+            if (decimal.TryParse(Console.ReadLine(), out decimal iKaKonvertuot))
+            {
+                return iKaKonvertuot;
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Blogai ivestas pasirinkimas!");
+                return IKaKonvertuoti(); Console.WriteLine("bad input. Try again:");
+                
+            }
+            //decimal input = decimal.TryParse(Console.ReadLine(), out decimal iKaKonvertuoti);
+            
         }
+        #endregion
     }
 }
