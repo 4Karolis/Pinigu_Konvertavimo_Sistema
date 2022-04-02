a3b5657acf94cd5fe77e05cab06efaf6fd738f21
4Karolis <4karolis.t@gmail.com>
2022-04-02 16:14:50 +0300

    Update Program.cs

--- Pninoiogu Konvertavimo Sistema/Program.cs
+++ Pninoiogu Konvertavimo Sistema/Program.cs
@@ -13,16 +13,17 @@ namespace Pninoiogu_Konvertavimo_Sistema
                 //1 EUR = 1,1126 USD, 1 EUR = 4,6679 PLN, 1 EUR = 22288,8906 BYR pagal 22.03.30 duomenis
                 decimal[] valiutuKursai = { 1.1126m, 4.6679m, 22288.8906m };
 
+                Meniu();
                 decimal iKaKonvertuot = IKaKonvertuoti();
                 switch (iKaKonvertuot)
                 {
-                    case 0:                        
+                    case 0:
                         Keisai(iKaKonvertuot, valiutuKursai[0], valiutosSimbolis[0]);
                         break;
-                    case 1:                        
+                    case 1:
                         Keisai(iKaKonvertuot, valiutuKursai[1], valiutosSimbolis[1]);
                         break;
-                    case 2:                       
+                    case 2:
                         Keisai(iKaKonvertuot, valiutuKursai[2], valiutosSimbolis[2]);
                         break;
                     case 3:
@@ -30,14 +31,23 @@ namespace Pninoiogu_Konvertavimo_Sistema
                         Console.Clear();
                         Console.WriteLine("Aciu, kad naudojates musu paslaugomis");
                         break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Blogai ivestas pasirinkimas! Rinkites dar karta");
+                    case 4:
+                        CloseApplication();
                         break;
+                    //default:
+                    //    Console.Clear();
+                    //    Console.WriteLine("Blogai ivestas pasirinkimas! Rinkites dar karta");
+                    //    break;
                 }
             }
         }
         #region METODAI        
+        private static void CloseApplication()
+        {
+            Console.WriteLine("Goodbey, thank you for using our Calculator");
+            Console.WriteLine("Press any key to close application....");
+            Environment.Exit(0);
+        }
         public static decimal Keisai(decimal iKaonvertuot, decimal valiutuKursai, string valiutosSimbolis)
         {
             //int p = int.Parse(Console.ReadLine());
@@ -76,21 +86,37 @@ namespace Pninoiogu_Konvertavimo_Sistema
         }
         public static decimal IKaKonvertuoti()
         {
+            decimal iKaKonvertuot, input;
+            while (true)
+            {                
+                decimal.TryParse(Console.ReadLine(), out decimal inputas);
+                bool gerasInputas = inputas == 1 || inputas == 2 || inputas == 3 || inputas == 4;
+                if (gerasInputas)
+                {
+                    iKaKonvertuot = inputas;
+                    break;
+                }
+                //else if (gerasInputas)
+                //{                    
+                //    iKaKonvertuot = inputas;
+                //    break;
+                //}
+                else
+                {
+                    Console.WriteLine("Bad input");
+                    //Console.ReadLine();
+
+                }                
+            }
+            return iKaKonvertuot;
+        }
+        public static void Meniu()
+        {
             Console.WriteLine("-----------------------------------------------------");
             Console.WriteLine("------------ PROGRAMA KONVERTUOJA EURUS -------------");
             Console.WriteLine("-----------------------------------------------------");
             Console.WriteLine("           I ka noretumete komnvertuoti?");
             Console.WriteLine("\n[0] USD | [1] PLN | [2] BYR | [3] ISEITI is programos");
-            if (decimal.TryParse(Console.ReadLine(), out decimal iKaKonvertuot))
-            {
-                return iKaKonvertuot;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Blogai ivestas pasirinkimas! Rinkites dar karta");
-                return IKaKonvertuoti();
-            }
         }
         #endregion
     }
